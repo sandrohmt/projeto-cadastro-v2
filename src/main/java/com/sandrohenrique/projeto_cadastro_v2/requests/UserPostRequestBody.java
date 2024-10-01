5a105167b8ea93a41f44b4b8024f195ce00c5b08
sandrohmt <sandrohmt@gmail.com>
2024-10-01 15:59:19 -0300

    Added some Validations

--- src/main/java/com/sandrohenrique/projeto_cadastro_v2/requests/UserPostRequestBody.java
+++ src/main/java/com/sandrohenrique/projeto_cadastro_v2/requests/UserPostRequestBody.java
@@ -10,11 +10,11 @@ public class UserPostRequestBody {
     @NotEmpty(message = "The email cannot be empty")
     @Email
     private String email;
-    @NotNull(message = "The age cannot be empty") // NotNull pois int e floats ja nao podem ser vazios
-    @Min(16)
+    @NotNull(message = "The age cannot be null") // NotNull pois int e floats ja nao podem ser vazios
+    @Min(18)
     @Max(120)
     private Integer age;
-    @NotNull(message = "The height cannot be empty") // NotNull pois int e floats ja nao podem ser vazios
+    @NotNull(message = "The height cannot be null") // NotNull pois int e floats ja nao podem ser vazios
     @Min(1)
     @Max(3)
     private Float height;
