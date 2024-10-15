b30808e3a8b2cf63be80313a6848b76a20cb0789
sandrohmt <sandrohmt@gmail.com>
2024-10-15 02:35:26 -0300

    Adding methods for creating user with certain validations

--- src/test/java/com/sandrohenrique/projeto_cadastro_v2/util/UserCreator.java
+++ src/test/java/com/sandrohenrique/projeto_cadastro_v2/util/UserCreator.java
@@ -21,4 +21,92 @@ public class UserCreator { // Usado para criar instâncias da entidade User
                 .height(1.75F)
                 .build();
     }
+
+    public static User createUserWithoutName() {
+        return User.builder()
+                .email("sandrohmt@gmail.com")
+                .age(21)
+                .height(1.75F)
+                .build();
+    }
+
+    public static User createUserWithShortName() {
+        return User.builder()
+                .name("Sandro")
+                .email("sandrohmt@gmail.com")
+                .age(21)
+                .height(1.75F)
+                .build();
+    }
+
+    public static User createUserWithLongName() {
+        return User.builder()
+                .name("Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu")
+                .email("sandrohmt@gmail.com")
+                .age(21)
+                .height(1.75F)
+                .build();
+    }
+
+    public static User createUserWithoutEmail() {
+        return User.builder()
+                .name("Sandro Henrique Matos Teixeira")
+                .age(21)
+                .height(1.75F)
+                .build();
+    }
+
+    public static User createUserWithoutAge() {
+        return User.builder()
+                .name("Sandro Henrique Matos Teixeira")
+                .email("sandrohmt@gmail.com")
+                .height(1.75F)
+                .build();
+    }
+
+
+    public static User createUserWithUnderage() {
+        return User.builder()
+                .name("Sandro Henrique")
+                .email("sandrohmt@gmail.com")
+                .age(17)
+                .height(1.75F)
+                .build();
+    }
+
+    public static User createUserWithAgeOver120() {
+        return User.builder()
+                .name("Sandro Henrique")
+                .email("sandrohmt@gmail.com")
+                .age(121)
+                .height(1.75F)
+                .build();
+    }
+
+    public static User createUserWithoutHeight() {
+        return User.builder()
+                .id(1L)
+                .name("Sandro Henrique Matos Teixeira")
+                .age(21)
+                .build();
+    }
+
+    public static User createUserWithHeightBelow60Centimeters() {
+        return User.builder()
+                .name("Sandro Henrique Matos Teixeira")
+                .email("sandrohmt@gmail.com")
+                .age(21)
+                .height(0.5F)
+                .build();
+    }
+
+    public static User createUserWithHeightAbove2_6Meters() {
+        return User.builder()
+                .name("Sandro Henrique Matos Teixeira")
+                .email("sandrohmt@gmail.com")
+                .age(21)
+                .height(2.7F)
+                .build();
+    }
 }
+
